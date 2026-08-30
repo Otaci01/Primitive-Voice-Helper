a9d1a6632180f83c96ae58a140a8ea58f2446a10
Otaci <123980283+Otaci01@users.noreply.github.com>
2026-08-31 02:26:07 +0300

    Update main.py
    новая функция открытия окна

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-import json, pyaudio
+import json, pyaudio, pygetwindow, webbrowser, subprocess, tkinter
 from vosk import Model, KaldiRecognizer
-import webbrowser
-import subprocess
-import tkinter
 from tkinter import messagebox
+import pygetwindow as gw
 
 #переменные
+gpk = "" #переменная с названием интересующего окна
 
 model = Model("Vosk\small-model") #путь к модели от Vosk его нужно будет изменить под себя
 rec = KaldiRecognizer(model, 16000)
@@ -17,6 +16,15 @@ stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, fram
 stream.start_stream()
 
 #функции
+def bring_to_front(gpk): #функция для открытия свёрнутого окна 
+    windows = gw.getWindowsWithTitle(gpk)
+    if windows:
+        win = windows[0]
+        if win.isMinimized:
+            win.restore()
+        win.activate()
+        return True
+    return False
 
 def listen():
     while True:
@@ -32,7 +40,9 @@ for text in listen():
         quit() #выход
     elif text =='гугл':
         webbrowser.open("https://www.google.com/?hl=ru") #открывает ссылку в браузере
-    elif text =='': #фраза запуска
+    elif text =='связь': #фраза запуска
         subprocess.Popen(r"") #открывает приложение(вставить полный путь до файла запуска), но не открывает окна уже запущенных приложений
+    elif text =='открыть': 
+        bring_to_front(gpk)    
     else:
         messagebox.showinfo("Помощник", text) #выдаёт текст в окне
